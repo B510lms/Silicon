9f731c94a364007d5375d4f558f1b85e5027f10b
B510lms <3020506015@qq.com>
2026-04-09 14:57:07 +0800

    fix: 解决vite.config.ts的报错

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 //引入svg需要用到插件
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 //mock插件提供方法
@@ -34,7 +31,7 @@ export default defineConfig(({ command, mode }) => {
       preprocessorOptions: {
         scss: {
           javascriptEnabled: true,
-          additionalData: `@import "${path.resolve(__dirname, './src/styles/variable.scss').replace(/\\/g, '/')}";`,
+          additionalData: '@use "@/styles/variable.scss" as *;',
         },
       },
     },
